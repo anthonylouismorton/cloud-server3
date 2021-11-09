200a2b874c4a65491f77cc620b7d4daef91407f4
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-11-08 17:54:05 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+const express = require('express') 
+const app = express(); 
+const PORT = process.env.PORT || 3010  
+app.use(express.static('./public'))  
+
+app.listen(PORT, () => {     
+  console.log('App is running on', {PORT})
+})
